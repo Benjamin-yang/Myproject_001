3f59b0d16bf08f116f0cabbbe514d43fe7fe7295
ben <1402202544@qq.com>
2018-08-29 18:11:30 +0800

    book的添加，删除

--- Class_001/lib/urls.py
+++ Class_001/lib/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('book/', views.book, name='book'),
     path('addBook', views.addBook, name='addBook'),
-
+    path('delBook/<int:book_id>', views.deleteBook, name='delBook'),
 ]
--- Class_001/lib/views.py
+++ Class_001/lib/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-
 # Create your views here.
 from django.urls import reverse
 
@@ -19,16 +18,20 @@ def book(request):
 
 
 def addBook(request):
-
     if request.method == 'POST':
         temp_name = request.POST['name']
         temp_author = request.POST['author']
         temp_pub_house = request.POST['pub_house']
 
-    from  django.utils import timezone
+    from django.utils import timezone
     temp_book = Book(name=temp_name, author=temp_author, pub_house=temp_pub_house,
                      pub_date=timezone.now())
     temp_book.save()
 
+    return HttpResponseRedirect(reverse("lib:book"))
 
+
+def deleteBook(request, book_id):
+    bookID = book_id
+    Book.objects.filter(id=book_id).delete()
     return HttpResponseRedirect(reverse("lib:book"))
