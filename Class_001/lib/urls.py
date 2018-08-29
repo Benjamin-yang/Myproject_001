c4a28a11185d4b4e996466b4721a91318910f5f9
ben <1402202544@qq.com>
2018-08-29 17:34:40 +0800

    玩成图书的增添

--- Class_001/lib/urls.py
+++ Class_001/lib/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'lib'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('book/', views.book, name='book'),
+    path('addBook', views.addBook, name='addBook'),
+
 ]
--- Class_001/lib/views.py
+++ Class_001/lib/views.py
@@ -1,7 +1,34 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+from django.urls import reverse
+
+from .models import Book
+
+
 def index(request):
     return HttpResponse('hello, world')
+
+
+def book(request):
+    book_list = Book.objects.order_by('-pub_date')[:5]
+    context = {"book_list": book_list}
+    return render(request, 'book.html', context=context)
+
+
+def addBook(request):
+
+    if request.method == 'POST':
+        temp_name = request.POST['name']
+        temp_author = request.POST['author']
+        temp_pub_house = request.POST['pub_house']
+
+    from  django.utils import timezone
+    temp_book = Book(name=temp_name, author=temp_author, pub_house=temp_pub_house,
+                     pub_date=timezone.now())
+    temp_book.save()
+
+
+    return HttpResponseRedirect(reverse("lib:book"))
